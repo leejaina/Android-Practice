2e9f1da365b91ebf8b320880106c4deecc7ffcfc
leejaina <leejaina@konkuk.ac.kr>
2025-02-10 14:53:33 +0900

    feat (study): Composable, Box
    -Composable: 내가 만든 함수~ UI 위해 구웠지~
    -Box: 포토샵 레이어를 코드로! 박스안의 박스도 가능하다. 비슷한 기능의 화면 요소끼리 묶어서 관리하자.

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.androidpractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
